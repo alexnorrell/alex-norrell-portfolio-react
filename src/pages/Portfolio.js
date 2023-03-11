cdb3dc50cb55a75c93d7e0b40f9ae2f91d3718fc
alexnorrell <norrell.alex@gmail.com>
2023-03-10 20:45:21 -0500

    started portfolio section

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,114 @@
+import "materialize-css/dist/css/materialize.min.css";
+
 export default function Portfolio () {
-    return (
-        <div>
-            <h1>Portfolio</h1>
+    return(
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                height: '60vh',
+                margin: '5%'
+            }}>
+            <div 
+                className="card-panel"
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    width: '100vw',
+                }}>
+                <div 
+                    className="row"
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-around'                 
+                    }}>
+                        
+                    <div className="col s2">
+                        <div className="card">
+                            <div className="card-image">
+                            <img 
+                                src="https://via.placeholder.com/200" alt='sample-img'/>
+                            <span className="card-title">Card Title</span>
+                            </div>
+                            <div className="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div className="card-action">
+                            <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col s2">
+                        <div className="card">
+                            <div className="card-image">
+                            <img 
+                                src="https://via.placeholder.com/200" alt='sample-img'/>
+                            <span className="card-title">Card Title</span>
+                            </div>
+                            <div className="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div className="card-action">
+                            <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div className="col s2">
+                        <div className="card">
+                            <div className="card-image">
+                            <img 
+                                src="https://via.placeholder.com/200" alt='sample-img'/>
+                            <span className="card-title">Card Title</span>
+                            </div>
+                            <div className="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div className="card-action">
+                            <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col s2">
+                        <div className="card">
+                            <div className="card-image">
+                            <img 
+                                src="https://via.placeholder.com/200" alt='sample-img'/>
+                            <span className="card-title">Card Title</span>
+                            </div>
+                            <div className="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div className="card-action">
+                            <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col s2">
+                        <div className="card">
+                            <div className="card-image">
+                            <img 
+                                src="https://via.placeholder.com/200" alt='sample-img'/>
+                            <span className="card-title">Card Title</span>
+                            </div>
+                            <div className="card-content">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div className="card-action">
+                            <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
